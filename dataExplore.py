6420f4e1c23ebf22d9e2772e08c8b9d5aea684f9
Xin Yao <susurrant@outlook.com>
2019-02-22 17:03:46 +0800

    Update dataExplore.py

--- dataExplore.py
+++ dataExplore.py
@@ -1,39 +1,42 @@
 import numpy as np
 import csv
 
-def process():
-    flows = {}
-    grids = {}
-    with open('data/unicom_OD.csv', 'r') as f:
+def unicom_data():
+    grid_map = {}
+    with open('data/250_is_500.csv', 'r') as f:
+        f.readline()
         line = f.readline()
+        while line:
+            d = line.strip().split(',')
+            grid_map[d[1]] = d[2] # map 250 to 500
+            line = f.readline()
+
+    flows_250 = {}
+    with open('data/unicom_OD.csv', 'r') as f:
+        f.readline()
         line = f.readline()
         while line:
             d = line.strip().split(',')
             if d[0] in ['20170109', '20170110', '20170111', '20170112', '20170113']:
-                if (d[2], d[3]) not in flows:
-                    flows[(d[2], d[3])] = 0
-                flows[(d[2], d[3])] += int(d[4])
-                if d[2] not in grids:
-                    grids[d[2]] = 0
-                if d[3] not in grids:
-                    grids[d[3]] = 0
+                if (d[2], d[3]) not in flows_250:
+                    flows_250[(d[2], d[3])] = 0
+                flows_250[(d[2], d[3])] += int(d[4])
             line = f.readline()
 
-    md = {}
-    for g, v in flows.items():
-        if v not in md:
-            md[v] = 0
-        md[v] += 1
-        if v > 100:
-            grids[g[0]] = 1
-            grids[g[1]] = 1
-    #print(md)
+    flows_500 = {}
+    for g, m in flows_250.items():
+        if g[0] in grid_map and g[1] in grid_map:
+            k = (grid_map[g[0]], grid_map[g[1]])
+            if k not in flows_500:
+                flows_500[k] = 0
+            flows_500[k] += m
 
-    with open('data/unicom_grid.csv', 'w', newline='') as f:
-        sheet = csv.writer(f)
-        sheet.writerow(['grid', 'tag'])
-        for grid, tag in grids.items():
-            sheet.writerow([grid, tag])
+    with open('data/unicom_500.csv', 'w', newline='') as rf:
+        sheet = csv.writer(rf)
+        sheet.writerow(['o', 'd', 'm'])
+        for g, m in flows_500.items():
+            sheet.writerow([g[0], g[1], m])
+
 
 if __name__ == '__main__':
-    process()
+    unicom_data()
