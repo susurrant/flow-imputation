cd2783daf3bd67220bd0f1d9d1b51d2290183a3b
Xin Yao <susurrant@outlook.com>
2019-02-27 11:35:23 +0800

    update codes

--- R-GCN/code/common/evaluation.py
+++ R-GCN/code/common/evaluation.py
@@ -315,7 +315,6 @@ class Scorer():
             print("Evaluating accuracies...")
 
         score_vector = self.model.score(triples)
-        print(score_vector, score_vector.shape)
         positives = score_vector[::2]
         negatives = score_vector[1::2]
 
