b139255d9bdcddc237ace689813fe34ee806969b
Xin Yao <susurrant@outlook.com>
2019-04-06 10:15:00 +0800

    update SI-GCN

--- SI-GCN/code/extras/spatial_representations.py
+++ SI-GCN/code/extras/spatial_representations.py
@@ -1,10 +1,12 @@
 
+import tensorflow as tf
 from model import Model
 import numpy as np
 
 
 class SpatialRepresentation(Model):
     shape = None
+    alpha = None
 
     def __init__(self, shape, settings, features, next_component=None):
         # next_component -> graph_representations.Representation(triples, encoder_settings), encoder_settings
@@ -12,14 +14,21 @@ class SpatialRepresentation(Model):
         self.shape = shape  # feature_shape = [int(encoder_settings['EntityCount']), int(encoder_settings['FeatureDimension'])]
         self.features = features.astype(np.float32)
 
+    def local_initialize_train(self):
+        initializer = np.random.random(size=self.shape).astype(np.float32)
+        self.alpha = tf.Variable(initializer)
+
+    def local_get_weights(self):
+        return [self.alpha]
+
     def get_all_subject_codes(self, mode='train'):
-        return self.features
+        return tf.pow(self.features, self.alpha)
 
     def get_all_object_codes(self, mode='train'):
-        return self.features
+        return tf.pow(self.features, self.alpha)
 
     def get_all_codes(self, mode='train'):
-        return self.features, None, self.features
+        return tf.pow(self.features, self.alpha), None, tf.pow(self.features, self.alpha)
 
     def print(self):
         print('layer type:', type(self))
