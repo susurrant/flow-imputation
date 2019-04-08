9309326a9935137e4659f7dd66c96c8b9d96da2a
Xin Yao <susurrant@outlook.com>
2019-04-08 17:01:53 +0800

    update NGM

--- neural gravity model.py
+++ neural gravity model.py
@@ -4,19 +4,6 @@ import tensorflow as tf
 import numpy as np
 from func import *
 
-
-# 构造添加神经层函数
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_function:
-        outputs = activation_function(Wx_plus_b)
-    else:
-        outputs = Wx_plus_b
-    return outputs
-
-
 def read_data(path, normalization=False):
     train_X = []
     train_y = []
@@ -42,34 +29,46 @@ def read_data(path, normalization=False):
             test_X.append([features[k[0]][3], features[k[1]][2],
                            dis(features[k[0]][0], features[k[0]][1], features[k[1]][0], features[k[1]][1])])
 
-    return np.array(train_X).reshape((-1, 3)), np.array(train_y).reshape((-1, 1)), np.array(test_X), np.array(test_y)
+    return np.array(train_X).reshape((-1, 3)), np.array(train_y).reshape((-1, 1)), \
+           np.array(test_X).reshape((-1, 3)), np.array(test_y).reshape((-1, 1))
 
 
-#if __name__ == '__main__':
-path = 'SI-GCN/data/taxi/'
-train_X, train_y, test_X, test_y = read_data(path, normalization=False)
-print(train_X.shape)
-print(train_y.shape)
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function:
+        outputs = activation_function(Wx_plus_b)
+    else:
+        outputs = Wx_plus_b
+    return outputs
 
-learn_rate = 0.01
-num_of_hidden_units = 20
 
-xs = tf.placeholder(tf.float32, shape = (None, 3))
-ys = tf.placeholder(tf.float32, shape = (None, 1))
+if __name__ == '__main__':
+    path = 'SI-GCN/data/taxi/'
+    train_X, train_y, test_X, test_y = read_data(path, normalization=True)
+    print(train_X.shape)
+    print(train_y.shape)
+    
+    learn_rate = 0.001
+    num_of_hidden_units = 20
 
-hidden_layer = add_layer(xs, 3, num_of_hidden_units, activation_function=tf.nn.sigmoid)
-prediction = add_layer(hidden_layer, num_of_hidden_units, 1, activation_function=None)
+    xs = tf.placeholder(tf.float32, shape = (None, 3))
+    ys = tf.placeholder(tf.float32, shape = (None, 1))
 
-loss = 0.5 * tf.reduce_sum(tf.square(ys - prediction))
-train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(loss)
+    hidden_layer = add_layer(xs, 3, num_of_hidden_units, activation_function=tf.nn.sigmoid)
+    prediction = add_layer(hidden_layer, num_of_hidden_units, 1, activation_function=None)
 
-with tf.Session() as sess:
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    for i in range(1000):
-        sess.run(train_step, feed_dict={xs: train_X, ys: train_y})
-        if i % 50 == 0:
-            print(sess.run(loss, feed_dict={xs: train_X, ys: train_y}))
+    loss = tf.reduce_mean(tf.square(ys - prediction))
+    train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(loss)
 
-    #pred = sess.run(prediction, feed_dict={xs:test_X})
-    #evaluate(pred, test_y)
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        for i in range(10000):
+            sess.run(train_step, feed_dict={xs: train_X, ys: train_y})
+            #if i % 100 == 0:
+            #    print(sess.run(loss, feed_dict={xs: train_X, ys: train_y}))
+
+        pred = sess.run(prediction, feed_dict={xs:test_X})
+        evaluate(pred.flatten().tolist(), test_y.flatten().tolist())
