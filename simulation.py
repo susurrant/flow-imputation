48cb9df2d90eacaf6464b0cbc6ec7ffe0ad9e299
Xin Yao <susurrant@outlook.com>
2019-04-23 15:09:27 +0800

    update gravity model, simulation and gcn_basis

--- simulation.py
+++ simulation.py
@@ -40,7 +40,7 @@ def plot(flows, regions, beta, K):
         x.append(np.log(dis(regions[f[0]].x, regions[f[0]].y, regions[f[1]].x, regions[f[1]].y)))
     x = np.array(x)
     y = np.array(y)
-
+    print(x)
     font1 = {'family': 'Arial', 'weight': 'normal', 'size': 14}
     font2 = {'family': 'Arial', 'weight': 'normal', 'size': 14}
     sns.set_style('whitegrid')
@@ -49,17 +49,17 @@ def plot(flows, regions, beta, K):
     lx = np.arange(1.4,2.9,0.1)
     plt.plot(lx, m * lx + b, linewidth=1, color='green')
 
-    #ny = np.random.normal(0, 0.1, y.shape[0]) + y
+    #ny = np.random.normal(0, 0.1, y.shape[0]) + y + 0.1
     #print(ny)
     ny = np.array([-2.74954513,-1.98675937,-2.81374532,-2.22458175,-2.03047404,-1.38728483,-1.94625147,
-                    -1.66228986,-2.00819296,-2.55600734])
+                    -1.66228986,-2.00819296,-2.55600734]) + 0.1
     l2 = plt.scatter(x, ny, 18, 'red', label='observed')
     nm, nb = np.polyfit(x, ny, 1)
     plt.plot(lx, nm * lx + nb, linewidth=1, color='red')
     plt.xlim(1.4, 2.8)
-    plt.ylim(-3, -1.2)
+    plt.ylim(-3, -1)
     x_ticks = np.arange(1.4, 3, 0.2)
-    y_ticks = np.arange(-3, -1, 0.3)
+    y_ticks = np.arange(-3, -0.5, 0.5)
     plt.xticks(x_ticks)
     plt.yticks(y_ticks)
     plt.xlabel(r'$\log\mathit{d}$', font1)
