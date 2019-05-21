892dc1916a377f905325ee48e85bf34e44b63798
Xin Yao <susurrant@outlook.com>
2019-05-21 15:33:28 +0800

    update baselines

--- baselines/radiation model.py
+++ baselines/radiation model.py
@@ -37,9 +37,9 @@ def predict(flows, features):
     return pred, real
 
 if __name__ == '__main__':
-    path = 'SI-GCN/data/taxi/'
+    path = './SI-GCN/data/taxi/'
     mode = 'positive'
     flows, features = read_data(path, False, mode)
     pred, real = predict(flows, features)
-    np.savetxt('data/pred_RM_negative.txt', pred, delimiter=',')
+    #np.savetxt('./data/pred_RM_negative.txt', pred, delimiter=',')
     evaluate(pred, real, mode)
