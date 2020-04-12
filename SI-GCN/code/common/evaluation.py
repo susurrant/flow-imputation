e099ca203ebc4153a0316c29931569fb234f75ed
Xin Yao <susurrant@outlook.com>
2020-04-12 16:40:55 +0800

    output RMSE & SMC

--- SI-GCN/code/common/evaluation.py
+++ SI-GCN/code/common/evaluation.py
@@ -113,10 +113,10 @@ class Scorer:
 
 
         self.iter += 50   # change the corresponding self.iter
-        if self.iter < 501:
+        if self.iter < 10001:
             self.RMSE.append(np.sqrt(np.mean(np.square(np.array(real)-np.array(pred)))))
             self.SMC.append(stats.spearmanr(np.array(real), np.array(pred))[0])
-        if self.iter == 500:
+        if self.iter == 10050:
             np.savetxt('../data/output/GCN_RMSE_th30.txt', np.array(self.RMSE), fmt='%.3f', delimiter=',')
             np.savetxt('../data/output/GCN_SMC_th30.txt', np.array(self.SMC), fmt='%.3f', delimiter=',')
 
